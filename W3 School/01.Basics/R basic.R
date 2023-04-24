feee57e7ad51c715c66d7ec5e8d3ee781c5bcd60
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-04-24 15:58:14 +0530

    added recursion

--- W3 School/01.Basics/R basic.R
+++ W3 School/01.Basics/R basic.R
@@ -103,3 +103,15 @@ my_function("Sweden")
 my_function("India")
 my_function() # will get the default value, which is Norway
 my_function("USA")
+
+#Recursion
+tri_recursion <- function(k) {
+  if (k > 0) {
+    result <- k + tri_recursion(k - 1)
+    print(result)
+  } else {
+    result = 0
+    return(result)
+  }
+}
+tri_recursion(6)
