78bb6bf369b876c3acb82e340a1ba75cee1c99e7
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-04-26 12:34:56 +0530

    lab 4 q2 part a done

--- SLIIT/04.Descriptive Statistics 2/lab 04.R
+++ SLIIT/04.Descriptive Statistics 2/lab 04.R
@@ -8,5 +8,20 @@ names(dataSet) <- c("Team","Attendance","Team Salary", "Years")
 fix(dataSet)
 
 attach(dataSet)
+#2
+#a
+#boxPlot
+boxplot(Attendance,main = "Box plot for Attendance", xlab= "Attendance",horizontal = TRUE,outpch = 19)
+boxplot(`Team Salary`, main = "Box Plot for Team Salary", xlab= "Team Salary", horizontal = TRUE,outpch = 19)
+boxplot(Years, main= "Box Plot for Years", xlab = "Years", horizontal = TRUE, outline = TRUE, outpch = 19)
 
+#histogram
+hist(Attendance, main = "Histogram for Attendance", ylab = "Frequency")
+hist(`Team Salary`, main = "Histogram for Team Salary", ylab = "Frequency")
+hist(Years, main = "Histogram for Years", ylab = "Frequency")
+
+#stem and leaf plots
+stem(Attendance)
+stem(`Team Salary`)
+stem(Years)
 
