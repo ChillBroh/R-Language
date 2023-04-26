998d220c582537431102454ed59d33b94c856fc4
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-04-26 16:37:58 +0530

    labsheet 5 done

--- SLIIT/05.Descriptive Statistics 3/lab 5.R
+++ SLIIT/05.Descriptive Statistics 3/lab 5.R
@@ -17,5 +17,71 @@ histogram <- hist(`ShareHolder Count`,main = "Histogram for the Number of Share 
 class(histogram)
 
 #Q3
+attach(histogram)
+#step 1 - identify breakpoints
+breaks
+
+#histogram$breaks
+#step 2 - identify frequency of each class
+counts
+
+#step 3 - Identify mid points of each class
+mids
+
+
+#create empty vecotr
+classes <- c()
+
+for(i in 1:length(breaks)-1){
+  classes[i] <- paste("{",breaks[i],"-",breaks[i+1],"}")
+}
+
+cbind(Classes = classes, Frequency= counts)
+help("cbind")
+
+#frequency polygon in same histogram plot
+lines(mids, counts)
+
+#draw in new plot
+plot(mids,counts,type = "l", main= "Frequency polygon for shareholders count",
+     xlab="Share holders", ylab="frequency", ylim = c(0,max(counts)))
+
+plot(mids,counts,type = "p", main= "Frequency polygon for shareholders count",
+     xlab="Share holders", ylab="frequency", ylim = c(0,max(counts)))
+
+plot(mids,counts,type = "o", main= "Frequency polygon for shareholders count",
+     xlab="Share holders", ylab="frequency", ylim = c(0,max(counts)))
+help("plot")
+
+#Q5
+#cumulative frequency
+cumulative <- cumsum(counts)
+cumulative
+
+cumPolygon <- c()
+
+for(i in 1:length(breaks)-1){
+  if(i == 0){
+    cumPolygon[i] = 0
+  }else{
+  cumPolygon[i] <- cumulative[i]
+  }
+}
+cbind(classes= classes, Frequency = cumulative)
+
+plot(breaks,cumPolygon,type = "l", main= "Frequency polygon for shareholders count",
+     xlab="Share holders", ylab="frequency", ylim = c(0,max(cumulative)))
+
+
+
+
+
+
+
+
+
+
+
+
 
 
