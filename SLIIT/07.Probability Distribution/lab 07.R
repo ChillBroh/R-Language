30a1450472473da85b51c4c2c7a4f7465e30c2b0
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-04-26 22:27:01 +0530

    lab 7 q 1 done

---
+++ SLIIT/07.Probability Distribution/lab 07.R
@@ -0,0 +1,13 @@
+#Q1
+#(i)
+X ~ Bin(44,0.92)
+
+#(ii)
+dbinom(40,44,0.92)
+?dbinom
+
+#(iii)
+pbinom(35,44,0.92)
+
+#(iv) - always get less than or equal for the calculations same as the calculator
+1 - pbinom(37,44,0.92)
