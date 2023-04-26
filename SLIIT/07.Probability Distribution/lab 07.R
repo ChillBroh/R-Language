696dec3354c55a4369102bbba68830f6c093c7b2
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-04-26 23:13:16 +0530

    question 3 done

--- SLIIT/07.Probability Distribution/lab 07.R
+++ SLIIT/07.Probability Distribution/lab 07.R
@@ -11,3 +11,51 @@ pbinom(35,44,0.92)
 
 #(iv) - always get less than or equal for the calculations same as the calculator
 1 - pbinom(37,44,0.92)
+
+#(v) x <= 42  and x >= 40 so x<=39
+pbinom(42,44,0.92) - pbinom(39,44,0.92)
+
+
+#Q2
+#(i)
+dpois(6,4.5)
+
+#(ii)
+ppois(6,4.5,lower.tail = FALSE) #less than is not relevent so lower tail = false
+
+1- ppois(6,4.5)
+
+
+#Q3
+#(i)
+pexp(3,1/2)
+?pexp
+
+#(ii)
+1- pexp(4,1/2)
+
+#(iii)
+pexp(4,1/2) - pexp(2,1/2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
